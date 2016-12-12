c89682eee2326dcfe4f6a86044cce892b2ba04fd
leryss <laur.h_97@yahoo.ro>
2016-12-12 15:32:24 +0200

    Revert "Revert "Added entity class & started resourceholder""
    This reverts commit 227432a39a08af7e52794f04dd4a7639e291eb30.

--- Tanks Classic/Data/Source/Main.cpp
+++ Tanks Classic/Data/Source/Main.cpp
@@ -1,7 +1,14 @@
 #include "Game.h"
+#include <exception>
+#include <iostream>
 
 int main()
 {
-	Game game;
-	game.run();
+	try {
+		Game game;
+		game.run();
+	}
+	catch (std::runtime_error& err) {
+		std::cout << err.what() << std::endl;
+	}
 }
